274741515172898cf8fe0b005126db61ea427145
Yuji181181 <s1f102402552@iniad.org>
2024-08-09 18:27:21 +0900

    基礎設定

--- Minesweeper.py
+++ Minesweeper.py
@@ -4,3 +4,18 @@ from tile import Tile
 from os import walk
 import random
 
+row_num = 20
+col_num = 20
+tile_size = 40
+bomb_num = 50
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+game_over = False
+game_clear = False
+
+timer = 0
